acb89d98cf4ad4191a1e1d4dbb01c2cecdbb5141
Gadgex <jaxfair02@gmail.com>
2018-07-25 11:49:07 -0600

    re-positioned the generate button

--- src/components/content.js
+++ src/components/content.js
@@ -6,6 +6,14 @@ class Content extends Component {
         super()
         this.count = 1;
     }
+
+    componentDidUpdate() {
+        this.count = 1;
+    }
+
+    componentWillReceiveProps() {
+        this.count = 1;
+    }
     renderLabel(data) {
         return [
             <div className="card__content__label">{this.count++}</div>,
